a165f515109f20eaed41e37a8d1454429081316b
Fernando Mattioli <fernando.mattioli@ebserh.gov.br>
2026-01-31 12:10:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Open Expense Manager Client
+
+
+## Creating a project
+
+
+```sh
+npx sv create
+```
+
+Choose options template minimal, typescript support, components prettier, eslint and tailwindcss (plugins typography and forms) and npm as the package manager.
+
+## Dependencies
+
+```sh
+npm i @lucide/svelte
+npm i -D @skeletonlabs/skeleton @skeletonlabs/skeleton-svelte
+```
+
+## Developing
+
+Run the project:
+
+```sh
+npm run dev
+```
+
+## Building
+
+To create a production version of your app:
+
+```sh
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
+
+> To deploy your app, you may need to install an [adapter](https://svelte.dev/docs/kit/adapters) for your target environment.
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { goto } from '$app/navigation';
+	import { AppBar, Menu, Portal, Toast } from '@skeletonlabs/skeleton-svelte';
+	import { LogOutIcon, MenuIcon } from '@lucide/svelte';
+	import { toaster } from '$lib/stores/toast';
+
+	let { children } = $props();
+
+	function logout() {
+		localStorage.removeItem("oemToken");
+		goto("/login");
+	}
+
+	function handleMenu(x:any) {
+		switch (x.value) {
+			case "x":
+				goto("/x");
+				break;
+			default:
+				goto("/home");
+		}
+	}
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<div class="grid h-screet grid-rows-[auto_1fr_auto]">
+	<AppBar>
+		<AppBar.Toolbar class="grid-cols-[auto_1fr_auto]">
+			<AppBar.Lead>
+				<Menu onSelect={handleMenu}>
+					<Menu.Trigger>
+						<button type="button" class="btn-icon btn-icon-lg hover:preset-tonal">
+							<MenuIcon />
+						</button>
+					</Menu.Trigger>
+					<Portal>
+						<Menu.Positioner>
+							<Menu.Content>
+								<Menu.Item value="optionA">
+									<Menu.ItemText>Option A</Menu.ItemText>
+								</Menu.Item>
+								<Menu.Item value="optionB">
+									<Menu.ItemText>Option B</Menu.ItemText>
+								</Menu.Item>
+							</Menu.Content>
+						</Menu.Positioner>
+					</Portal>
+				</Menu>
+			</AppBar.Lead>
+			<AppBar.Headline>
+				<p class="text-2xl">Open Expense Manager</p>
+			</AppBar.Headline>
+			<AppBar.Trail>
+				<button type="button" class="btn-icon hover:preset-tonal">
+					<LogOutIcon class="size-6" onclick={logout} />
+				</button>
+			</AppBar.Trail>
+		</AppBar.Toolbar>
+	</AppBar>
+
+	<div class="grid grid-cols-1 md:grid-cols-[auto_1fr]" style="min-height:80vh">
+		<main class="space-y-4 p-4">
+			<div style="display: contents">{@render children()}</div>
+		</main>
+	</div>
+
+	<Toast.Group {toaster}>
+		{#snippet children(toast)}
+			<Toast {toast}>
+				<Toast.Message>
+					<Toast.Title>{toast.title}</Toast.Title>
+					<Toast.Description>{toast.description}</Toast.Description>
+				</Toast.Message>
+				<Toast.CloseTrigger />
+			</Toast>
+		{/snippet}
+	</Toast.Group>
+
+	<footer class="bg-gray-200 p-4">2026 | github.com/f-matt</footer>
+</div>
